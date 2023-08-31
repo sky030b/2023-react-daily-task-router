1a976a7b407e983ca1c95e33895834f445af37a9
Sky <james156489723@gmail.com>
2023-09-01 01:48:36 +0800

    update Dynamic Route function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, NavLink, useNavigate } from 'react-router-dom';
+import { HashRouter, NavLink, useNavigate, Outlet, useParams } from 'react-router-dom';
 import { Routes, Route } from 'react-router'
 import { Fragment } from 'react';
 
@@ -7,6 +7,21 @@ import { Fragment } from 'react';
 const LogOut = () => {
   const navigate = useNavigate();
   return <button onClick={() => {navigate('/login')}}>登出</button>
+}
+
+// 父層元件
+const Post = () => {
+  return <div>
+    <h3>Post 頁面</h3>
+    <Outlet />
+  </div>
+}
+
+// 子層元件
+const PostId = () => {
+  let params = useParams();
+  params.userId = "xxxx"
+  return <p>Post: {params.userId}</p>;
 }
 
 const Todo = () => {
@@ -41,12 +56,21 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/post123">
+            <p>Post 詳細頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
           <Route
             path="*"
             element={
